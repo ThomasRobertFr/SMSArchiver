db56c43d4ff3f22c7b3fa661cc749c06d6bc8c88
Antoine Augusti <antoine@augusti.fr>
2014-07-22 16:55:53 +0200

    Fix missing word in license

--- initialImport.php
+++ initialImport.php
@@ -2,7 +2,7 @@
 
 /*  Author: Thomas Robert - thomas-robert.fr - Github @ThomasRobertFr
     
-    This file is part SMSArchiver.
+    This file is part of SMSArchiver.
 
     SMSArchiver is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- lib/Views.php
+++ lib/Views.php
@@ -1,7 +1,7 @@
 <?php
 /*  Author: Thomas Robert - thomas-robert.fr - Github @ThomasRobertFr
     
-    This file is part SMSArchiver.
+    This file is part of SMSArchiver.
 
     SMSArchiver is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
